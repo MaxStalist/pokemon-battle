5665e3573dff33767c58328f1596ce0efb2aa6e9
maxstalist <aramstalist@gmail.com>
2022-02-03 14:14:49 +0200

    first commit

---
+++ pokemons.js
@@ -0,0 +1,194 @@
+export const pokemons = [
+    {
+        name: 'Pikachu',
+        type: 'electric',
+        hp: 320,
+        img: 'http://sify4321.000webhostapp.com/pikachu.png',
+        attacks: [
+            {
+                name: "Thunder Shock",
+                maxDamage: 40,
+                minDamage: 30,
+                maxCount: 30,
+            },
+            {
+                name: "Discharge",
+                maxDamage: 80,
+                minDamage: 50,
+                maxCount: 12,
+            },
+            {
+                name: "Thunderbolt",
+                maxDamage: 90,
+                minDamage: 60,
+                maxCount: 15,
+            },
+            {
+                name: "Thunder",
+                maxDamage: 110,
+                minDamage: 70,
+                maxCount: 10,
+            }
+        ]
+    },
+    {
+        img: 'http://sify4321.000webhostapp.com/charmander.png',
+        name: 'Charmander',
+        type: 'fire',
+        hp: 309,
+        attacks: [
+            {
+                name: "Fire Fang",
+                maxDamage: 65,
+                minDamage: 30,
+                maxCount: 15,
+            },
+            {
+                name: "Flamethrower",
+                maxDamage: 90,
+                minDamage: 60,
+                maxCount: 15,
+            },
+            {
+                name: "Fire Spin",
+                maxDamage: 85,
+                minDamage: 35,
+                maxCount: 15,
+            },
+            {
+                name: "Inferno",
+                maxDamage: 100,
+                minDamage: 50,
+                maxCount: 5,
+            }
+        ]
+    },
+    {
+        img: 'http://sify4321.000webhostapp.com/bulbasaur.png',
+        name: 'Bulbasaur',
+        type: 'fire',
+        hp: 318,
+        attacks: [
+            {
+                name: "Vine Whip",
+                maxDamage: 45,
+                minDamage: 20,
+                maxCount: 25,
+            },
+            {
+                name: "Razor Leaf",
+                maxDamage: 55,
+                minDamage: 30,
+                maxCount: 25,
+            },
+            {
+                name: "Seed Bomb",
+                maxDamage: 80,
+                minDamage: 60,
+                maxCount: 15,
+            },
+            {
+                name: "Double Edge",
+                maxDamage: 120,
+                minDamage: 100,
+                maxCount: 15,
+            }
+        ]
+    },
+    {
+        img: 'http://sify4321.000webhostapp.com/squirtle.png',
+        name: 'Squirtle',
+        type: 'water',
+        hp: 314,
+        attacks: [
+            {
+                name: "Bubble",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 30,
+            },
+            {
+                name: "Water Pulse",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 20,
+            },
+            {
+                name: "Aqua Tail",
+                maxDamage: 90,
+                minDamage: 60,
+                maxCount: 10,
+            },
+            {
+                name: "Hydro Pump",
+                maxDamage: 110,
+                minDamage: 80,
+                maxCount: 5,
+            }
+        ]
+    },
+    {
+        img: 'http://sify4321.000webhostapp.com/pidgey.png',
+        name: 'Pidgey',
+        type: 'air',
+        hp: 251,
+        attacks: [
+            {
+                name: "Twister",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 20,
+            },
+            {
+                name: "Wing Attack",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 35,
+            },
+            {
+                name: "Air Slash",
+                maxDamage: 75,
+                minDamage: 60,
+                maxCount: 15,
+            },
+            {
+                name: "Hurricane",
+                maxDamage: 110,
+                minDamage: 70,
+                maxCount: 10,
+            }
+        ]
+    },
+    {
+        img: 'http://sify4321.000webhostapp.com/mew.png',
+        name: 'Mew',
+        type: 'air',
+        hp: 290,
+        attacks: [
+            {
+                name: "Scratch",
+                maxDamage: 40,
+                minDamage: 20,
+                maxCount: 35,
+            },
+            {
+                name: "Bite",
+                maxDamage: 60,
+                minDamage: 45,
+                maxCount: 25,
+            },
+            {
+                name: "Slash",
+                maxDamage: 70,
+                minDamage: 60,
+                maxCount: 20,
+            },
+            {
+                name: "Night Slash",
+                maxDamage: 70,
+                minDamage: 50,
+                maxCount: 15,
+            }
+        ]
+    }
+];
